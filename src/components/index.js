6847a8db37f37cbe04f6bcecccbf8676327ccb88
Maxim Roschin <maks.roshin2004@gmail.com>
2026-06-04 01:24:00 +0300

    ok

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export * from './private-route/private-route';
 export * from './layout/Layout';
-export * from './h2/H2';
+export * from './h2/h2';
 export * from './auth-form-error/auth-form-error';
 export * from './button/button';
 export * from './input/input';
